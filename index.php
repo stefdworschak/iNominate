d63f0d31f01ec49a970b463e69fd642d7bf52bac
stefdworschak <stef.dworschak@gmail.com>
2018-09-20 21:05:22 +0100

    testing msql connection with heroku

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
-    require('init.php')
-    echo "Hello, World 2018!";
+    require('init.php');
+    //echo "Hello, World 2018!";
+
 ?>
--- init.php
+++ init.php
@@ -1,3 +1,30 @@
-function init(){
+<?php
 
-}
+
+
+    //Heroku Production Details
+
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    $conn = new mysqli($server, $username, $password, $db);
+
+
+/*
+  $server = 'localhost';
+  $user = 'root';
+  $pw = 'root';
+
+  $conn = new mysqli($server, $user);
+*/
+  if($conn->connect_error){
+    die('Connection failed' . $conn->connect_error);
+  }
+
+  echo "Connection established successfully!";
+
+ ?>
