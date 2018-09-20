ad1e554543b7902dcf6faa1d7211700483b68849
stefdworschak <stef.dworschak@gmail.com>
2018-09-20 21:14:20 +0100

    creating users table

--- init.php
+++ init.php
@@ -11,20 +11,39 @@
     $password = $url["pass"];
     $db = substr($url["path"], 1);
 
+    /*
+      // Localhost Dev Details
+
+      $server = 'localhost';
+      $user = 'root';
+      $pw = 'root';
+
+      $conn = new mysqli($server, $user);
+    */
+
+
     $conn = new mysqli($server, $username, $password, $db);
 
+    if($conn->connect_error){
+      die('Connection failed ' . $conn->connect_error);
+    }
 
-/*
-  $server = 'localhost';
-  $user = 'root';
-  $pw = 'root';
+    echo "Connection established successfully!";
 
-  $conn = new mysqli($server, $user);
-*/
-  if($conn->connect_error){
-    die('Connection failed' . $conn->connect_error);
-  }
+    $sql = "CREATE TABLE IF NOT EXISTS users (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        first_name VARCHAR(32) NOT NULL,
+        last_name VARCHAR(32) NOT NULL,
+        email_address VARCHAR(50) NOT NULL,
+        user_type VARCHAR(32) NOT NULL,
+        registration_date TIMESTAMP
+    );"
 
-  echo "Connection established successfully!";
+    if($conn->query($sql) === TRUE){
+      echo "Table created successfully!";
+    } else {
+      die('Error in SQL Syntax ' . $conn->connect_error);
+    }
+
 
  ?>
