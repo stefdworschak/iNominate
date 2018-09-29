cc421f6be699fdbe189935b3a95fc481a316d96a
stefdworschak <stef.dworschak@gmail.com>
2018-09-29 19:43:13 +0100

    some changes

--- core/functions/connect.php
+++ core/functions/connect.php
@@ -1,6 +1,15 @@
 <?php
 //require('core/env_vars.php');
-require('create_hash.php');
+//require('create_hash.php');
+
+function createHash($pwd) {
+  $options = [
+      'cost' => 11,
+      'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
+  ];
+  //echo password_hash("Password1", PASSWORD_BCRYPT, $options);
+  return password_hash($pwd, PASSWORD_BCRYPT, $options);
+}
 
 class DBClass extends DBSettings
 {
