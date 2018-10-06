e125de91865863e0b088e5cf2a576ee36b9d1b6c
stefdworschak <stef.dworschak@gmail.com>
2018-10-06 14:10:59 +0100

    added some error handling on the registration

--- core/js/index_script.js
+++ core/js/index_script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  var $orgs = null;
   $('#registerBtn').click(function(){
     event.preventDefault();
     window.location.href="index.php?mode=register";
@@ -29,11 +30,51 @@ $(document).ready(function(){
       function callback(res){
         console.log(res);
         var $src = JSON.parse(res);
-        $('#org').autocomplete({ source: $src })
+        $('#org').autocomplete({ source: $src });
+        $orgs = res;
       }
 
+    } else {
+      $orgs = null;
     }
 
+  });
+
+  $('#regBtn').click(function(){
+      event.preventDefault();
+      var $errs = $('.err_output')
+      $errs.text('');
+    /*  $errs.each(function(){
+        $(this).text("");
+      })*/
+      var $email = $('#email').val();
+      var $first = $('#FirstName').val();
+      var $last = $('#LastName').val();
+      var $usertype = $('#UserType').val();
+      var $org = $('#org').val() == undefined ? '' : $('#org').val();
+      var $pwd = $('#Password').val();
+      var $pwd2 = $('#Password1').val();
+      //console.log($usertype)
+      //console.log(!$org)
+      //console.log($orgs.indexOf('') == -1)
+      //console.log(!org || ($orgs != null && $orgs.indexOf($org) == -1))
+      if(!$email || !$first || !$last || $usertype == 'Please Select' || !$org || !$pwd || !$pwd2){
+        if(!$email) { $('#email_err').text("Please enter an email address."); }
+        if(!$first) { $('#last_err').text("Please enter your first name"); }
+        if(!$last) { $('#first_err').text("Please enter your last name."); }
+        if($usertype == 'Please Select') { $('#usertype_err').text("Please select a user type."); }
+        if(!$org) { $('#org_err').text("Please enter your organization."); }
+        if(!$pwd) { $('#pwd_err').text("Please enter a password."); }
+        if(!$pwd2) { $('#pwd2_err').text("Please confirm your password."); }
+      } else if($pwd != $pwd2){
+        console.log($pwd)
+        console.log($pwd2)
+          $('#pwd2_err').text("Your passwords do not match.");
+      } else if($orgs != null && $orgs.indexOf($org) == -1) {
+        $('#org_err').text("Please select from the list of existing companies or register your company as admin!");
+      } else {
+        $('#RegForm').submit();
+      }
   });
 
   $('.my_profile_img').hover(function(){
--- core/views/register.php
+++ core/views/register.php
@@ -7,22 +7,22 @@
       <br />
         <h1>Register</h1>
         <!-- Source: https://getbootstrap.com/docs/4.0/components/forms/ -->
-        <form method="POST" action="core/functions/register_script.php" enctype="multipart/form-data">
+        <form method="POST" action="core/functions/register_script.php" enctype="multipart/form-data" id="RegForm">
           <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" name="emailaddress" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <label for="email">Email address<span class="err_output" id="email_err"><span></label>
+            <input type="email" class="form-control" name="emailaddress" id="email" aria-describedby="emailHelp" placeholder="Enter email">
             <!--<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>-->
           </div>
           <div class="form-group">
-            <label for="FirstName">First Name</label>
+            <label for="FirstName">First Name<span class="err_output" id="first_err"><span></label>
             <input type="text" class="form-control" name="first_name" id="FirstName" aria-describedby="emailHelp" placeholder="First Name">
           </div>
           <div class="form-group">
-            <label for="LastName">Last Name</label>
+            <label for="LastName">Last Name<span class="err_output" id="last_err"><span></label>
             <input type="text" class="form-control" name="last_name" id="LastName" aria-describedby="emailHelp" placeholder="Last Name">
           </div>
           <div class="form-group">
-            <label for="UserType">User Type</label>
+            <label for="UserType">User Type<span class="err_output" id="usertype_err"><span></label>
             <br />
             <select id="UserType" name="user_type">
                 <option>Please Select</option>
@@ -31,19 +31,20 @@
             </select>
           </div>
           <div class="form-group" id="orgDisplay">
-            <label for="org">Organisation Name</label>
+            <label for="org">Organisation Name<span class="err_output" id="org_err"><span></label>
             <input type="text" class="form-control" name="org" id="org" placeholder="Organisation Name">
           </div>
           <div class="form-group">
-            <label for="Password">Password</label>
+            <label for="Password">Password<span class="err_output" id="pwd_err"><span></label>
             <input type="password" name="password" class="form-control" id="Password" placeholder="Password">
           </div>
           <div class="form-group">
-            <label for="Password1">Repeat Password</label>
+            <label for="Password1">Repeat Password<span class="err_output" id="pwd2_err"><span></label>
             <input type="password" name="password1" class="form-control" id="Password1" placeholder="Repeat Password">
           </div>
-          <button type="submit" class="btn btn-primary">Register</button>&nbsp;
+          <button type="submit" id="regBtn" class="btn btn-primary">Register</button>&nbsp;
           <button id="loginBtn" class="btn btn-danger">Sign In</button>
+          <div style="display:inline;" id="err_output"></div>
         </form>
         <br />
       </div>
