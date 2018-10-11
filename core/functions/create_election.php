c51c4fe5b78cd143e70d63219fd9560604dca99b
stefdworschak <stef.dworschak@gmail.com>
2018-10-11 19:35:47 +0100

    changed header

--- core/functions/create_election.php
+++ core/functions/create_election.php
@@ -7,7 +7,7 @@
       $c = new DBClass;
       $result = $c->createElection($_POST);
       echo $result;
-      //header('Location:../../index.php?view=admin_panel');
+      header('Location:../../index.php?view=admin_panel');
     } else {
       header('Location:../../index.php?view=create_election?err=db_error');
     }
