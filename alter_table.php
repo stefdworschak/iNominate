610cbad80ec89f7179b300c84017aea71f70cb90
stefdworschak <stef.dworschak@gmail.com>
2018-10-11 19:34:53 +0100

    changing elections table

--- alter_table.php
+++ alter_table.php
@@ -5,13 +5,19 @@
     $c->connect();
     //$stmt=$c->conn->prepare("ALTER TABLE `users` ADD `password` VARCHAR(100) AFTER `email_address`;");
 
-    $str = "CREATE TABLE IF NOT EXISTS `elections` (`id` int(11) NOT NULL,`title` varchar(50) NOT NULL, ";
+    $str = "DROP TABLE `elections`;";
+    $stmt=$c->conn->prepare($str);
+    $stmt->execute();
+
+    $str = "CREATE TABLE IF NOT EXISTS `elections3` (`id` int(11) NOT NULL AUTO_INCREMENT,";
+    $str .= "`title` varchar(50) NOT NULL, ";
     $str .= "`description` tinytext NOT NULL,";
     $str .= "`department` varchar(32) NOT NULL,";
     $str .= "`num_candidates` int(6) NOT NULL,";
     $str .= "`num_roles` int(6) NOT NULL,";
     $str .= "`expiry_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,";
-    $str .= "`creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP";
+    $str .= "`creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,";
+    $str .= "PRIMARY KEY(id)";
     $str .= ");";
 
     $stmt=$c->conn->prepare($str);
