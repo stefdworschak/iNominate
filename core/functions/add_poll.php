6ccdd8d15a943368965115c9e8f2f7723dec109f
stefdworschak <stef.dworschak@gmail.com>
2018-12-11 18:33:10 +0000

    final changes

--- core/functions/add_poll.php
+++ core/functions/add_poll.php
@@ -5,10 +5,8 @@
       session_start();
       $c = new DBClass;
       $result = $c->createPoll($_POST, $_SESSION['userid'], $_SESSION['department']);
-      if($result > 0) {
           //$c->emailPoll($_POST['poll_id'], $_SESSION['department']);
           header('Location:../../index.php');
-      }
 
     } else {
       header('Location:../../index.php?view=create_election?err=db_error');
