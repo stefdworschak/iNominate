ced098d581db763d59ab8647991b4c7e8de2aca1
stefdworschak <stef.dworschak@gmail.com>
2018-12-11 00:34:10 +0000

    otp check

--- core/functions/otp.php
+++ core/functions/otp.php
@@ -1,5 +1,5 @@
 <?php
-
+require('email.php');
 function generateOTP($n, $env_path, $e_path)
 {
     $Generator = "135792468";
@@ -470,15 +470,15 @@ function sendOTP($OTP, $env, $e_path){
    $headers .= 'From: Mailer <inominate18@gmail.com>' . "\r\n";
    $headers .= 'X-Mailer: PHP/' . phpversion();
    try{
-        $email_response =  sendEmail($to, $subject, $message);
-  /*     $email_response = $e_path == 0 ? mail($to, $subject, $message, $headers) : sendEmail($to, $subject, $message);
+        //$email_response =  sendEmail($to, $subject, $message);
+       $email_response = $e_path == 0 ? mail($to, $subject, $message, $headers) : sendEmail($to, $subject, $message);
 
        // if(!mail($to, $subject, $message, $headers)){
        if(!$email_response || $email_response != 202){
          echo "Error !!";
         } else{
          //echo "Email Sent !!";
-       }*/
+       }
   } catch(Exception $e){
       echo $e->getMessage();
   }
